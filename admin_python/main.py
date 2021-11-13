4a6b9c15e2c4583af11cfacaeb4279a3e19aabbf
TSKim <kimtaesoo188@gmail.com>
2021-11-14 08:21:25 +0900

    Python Admin 추가

---
+++ admin_python/main.py
@@ -0,0 +1,42 @@
+
+
+import glob, os
+import json
+from google.cloud import firestore
+import hashlib
+
+
+def upload_data(data):
+    db = firestore.Client()
+
+    choices = data['choices']
+    question = data['question']
+    tags = data['tags']
+
+    hash_key = hashlib.sha256()
+    hash_key.update(data['question']['text'].encode('utf-8'))
+    hash_key.update(''.join(data['tags']).encode('utf-8'))
+    hash_key.update(''.join([c['answer'] for c in data['choices']]).encode('utf-8'))
+    print()
+
+    tagRefs = [db.collection('tags').document(tag) for tag in data['tags']]
+    # print(tagRefs)
+    doc_ref = db.collection('build_up').document(hash_key.hexdigest())
+    doc_ref.set({
+        'question': data['question'],
+        'choice': data['choices'],
+        'tags': tagRefs
+    })
+
+
+if __name__ == '__main__':
+    for root, dirs, files in os.walk("Staging"):
+
+        for file in files:
+            if file.endswith(".json"):
+                file_name = root + "/" + file
+                print("open file name: ", file_name)
+                with open(file_name, encoding='utf-8', errors='ignore') as json_data:
+                    data = json.load(json_data, strict=False)
+                    print(data)
+                    upload_data(data)
